d2cf3625521c7d391d3dba407124d5909ca381cb
HuzurEnderun <106527229+HuzurEnderun@users.noreply.github.com>
2022-12-30 10:55:30 +0300

    Add files via upload

---
+++ a/main.js
@@ -0,0 +1,42 @@
+(function () {
+  const second = 1000,
+    minute = second * 60,
+    hour = minute * 60,
+    day = hour * 24;
+
+  let today = new Date(),
+    dd = String(today.getDate()).padStart(2, "0"),
+    mm = String(today.getMonth() + 1).padStart(2, "0"),
+    yyyy = today.getFullYear(),
+    nextYear = 2023,
+    dayMonth = "01/17/",
+    sayac = dayMonth + yyyy;
+
+  today = mm + "/" + dd + "/" + yyyy;
+  if (today > sayac) {
+    sayac = dayMonth + nextYear;
+  }
+
+  const countDown = new Date(sayac).getTime(),
+    x = setInterval(function () {
+      const now = new Date().getTime(),
+        distance = countDown - now;
+
+      (document.getElementById("days").innerText = Math.floor(distance / day)),
+        (document.getElementById("hours").innerText = Math.floor(
+          (distance % day) / hour
+        )),
+        (document.getElementById("minutes").innerText = Math.floor(
+          (distance % hour) / minute
+        )),
+        (document.getElementById("seconds").innerText = Math.floor(
+          (distance % minute) / second
+        ));
+      if (distance < 0) {
+        document.getElementById("headline").innerText = "Huzur Enderun";
+        document.getElementById("countdown").style.display = "none";
+        document.getElementById("content").style.display = "block";
+        clearInterval(x);
+      }
+    }, 0);
+})();
